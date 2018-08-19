377d2f43102c14d89a7f5557fcab558b7ca25a78
Jordi Joan <jordi.joan@gmail.com>
2018-08-19 22:16:09 +0100

    Exercise 4 including curves.

--- main.r
+++ main.r
@@ -18,6 +18,9 @@ ptextsize <- 20
 species <- c("Species.1", "Species.2", "Species.3", "Species.4", "Species.5", "Species.6", "Species.7")
 species_labels <- c("Average", "Species 1", "Species 2", "Species 3", "Species 4", "Species 5", "Species 6", "Species 7")
 palette <- c("#000000", "#ff0000", "#ceb600", "#048c4e", "#007cff", "#ffa72d", "#b05bff", "#41ff1c")
+species_labels_wo_avg <- c("Species 1", "Species 2", "Species 3", "Species 4", "Species 5", "Species 6", "Species 7")
+palette_wo_avg <- c("Species 1" = "#ff0000", "Species 2" = "#ceb600", "Species 3" = "#048c4e", "Species 4" = "#007cff", "Species 5" = "#ffa72d", "Species 6" = "#b05bff", "Species 7" = "#41ff1c")
+palette2 <- c("#ff0000", "#ceb600", "#048c4e", "#007cff", "#ffa72d", "#b05bff", "#41ff1c")
 
 InfoDataset <- function(data, output) {
     # find species distribution when lost landscape is 0.25 0.8 and 0.9
@@ -36,7 +39,7 @@ InfoDataset <- function(data, output) {
         geom_line(aes(linetype=Species), size=1) +
         scale_linetype_manual(guide=FALSE, values=c("dashed", "solid", "solid", "solid", "solid", "solid", "solid", "solid"))+
         ggtitle("")  +
-        labs(title="", x="Lost landscape", y="Remaining distributions", color="") +
+        labs(title="", x="Lost landscape", y="Remaining distribution", color="") +
         scale_x_continuous(breaks=c(0, 0.25, 0.5, 0.75, 1), expand=c(0, 0)) +
         scale_y_continuous(breaks=c(0, 0.25, 0.5, 0.75, 1), expand=c(0, 0)) +
         #scale_colour_manual(labels=species_labels, values=palette) +
@@ -67,6 +70,8 @@ dset2 <- read.table(file="./data/exercise2.csv", sep=",", header=TRUE)
 InfoDataset(dset2, "./plots/exercise2.png")
 dset3 <- read.table(file="./data/exercise3.csv", sep=",", header=TRUE)
 InfoDataset(dset3, "./plots/exercise3.png")
+dset4 <- read.table(file="./data/exercise4.csv", sep=",", header=TRUE)
+InfoDataset(dset4, "./plots/exercise4.png")
 
 # Compare exercise 1 and exercise 2 with a plot facet wrap by species
 # prepare data
@@ -95,7 +100,7 @@ pplot <- ggplot(dtall12, aes(Proportion, Value, color=exercise)) +
     geom_line(size=1) + 
     facet_wrap(~ Species, ncol = 4) +
     ggtitle("")  +
-    labs(title="", x="Lost landscape", y="Remaining distributions", color="") +
+    labs(title="", x="Lost landscape", y="Remaining distribution", color="") +
     scale_x_continuous(breaks=c(0, 0.25, 0.5, 0.75, 1), expand=c(0, 0)) +
     scale_y_continuous(breaks=c(0, 0.25, 0.5, 0.75, 1), expand=c(0, 0)) +
     scale_colour_manual(labels=c("Exercise 1", "Exercise 2"), values=c("#ff0000", "#048c4e")) +
@@ -122,7 +127,7 @@ pplot <- ggplot(dtall23, aes(Proportion, Value, color=exercise)) +
     geom_line(size=1) + 
     facet_wrap(~ Species, ncol = 4) +
     ggtitle("")  +
-    labs(title="", x="Lost landscape", y="Remaining distributions", color="") +
+    labs(title="", x="Lost landscape", y="Remaining distribution", color="") +
     scale_x_continuous(breaks=c(0, 0.25, 0.5, 0.75, 1), expand=c(0, 0)) +
     scale_y_continuous(breaks=c(0, 0.25, 0.5, 0.75, 1), expand=c(0, 0)) +
     scale_colour_manual(labels=c("Exercise 2", "Exercise 3"), values=c("#ff0000", "#048c4e")) +
@@ -148,3 +153,27 @@ remove(dmelt1)
 remove(dmelt2)
 remove(dtall23)
 remove(dmelt3)
+
+# BQP Curves Eercise 4
+dsetc <- read.table(file="./data/bqp_curves_exercise4.csv", sep=",", header=TRUE)
+pplot <- ggplot(dsetc, aes(X, Y, color=Species)) +
+    geom_line(size=1) +
+    ggtitle("")  +
+    labs(title="", color="") +
+    scale_x_continuous(name="Proportion of neighboring cells lost", breaks=c(0, 0.25, 0.5, 0.75, 1), expand=c(0, 0)) +
+    scale_y_continuous(name="Local value remaining", breaks=c(0, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2), expand=c(0, 0)) +
+    scale_colour_manual(values=palette_wo_avg) +
+    theme(plot.title=element_text(hjust=0.5),
+        axis.title.x=element_text(size=ptextsize, family=pfamily),
+        axis.title.y=element_text(size=ptextsize, family=pfamily),
+        axis.text=element_text(size=ptextsize-2 , family=pfamily),
+        text=element_text(size=ptextsize, family=pfamily),
+        legend.spacing=unit(0, "cm"),
+        rect=element_blank()) +
+    theme(axis.line.x=element_line(color="grey", size=0.5),
+        axis.line.y=element_line(color="grey", size=0.5)) +
+    theme(panel.grid=element_blank(), panel.border=element_blank())
+
+png(file="./plots/exercise4_bqp_curves.png", width=800, height=500)
+print(pplot)
+dev.off()
